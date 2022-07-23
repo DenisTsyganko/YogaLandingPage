47e34eeb356816783b43e774305903add3c43623
Denis <tsyganko_denis@mail.ru>
2022-07-23 19:05:02 +0300

    Added modal windows

--- js/script.js
+++ js/script.js
@@ -88,4 +88,32 @@ window.addEventListener('DOMContentLoaded', function(){
     }
 
     setClock('timer',deadLine);
+
+    // modal
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click',function(){
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function(){
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
+    //modal window for header additional info
+    let infoDiv = document.querySelector('.info');
+    infoDiv.addEventListener('click', function(event){
+        let target = event.target;
+        if (target && target.classList.contains('description-btn')){
+            overlay.style.display = 'block';
+            more.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        }
+    });
 });
